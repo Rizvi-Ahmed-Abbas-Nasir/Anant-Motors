0507934bdabdcede7dcfa5d075862e3df449ddcf
Rizvi Ahmed Abbas <abbasrizvi200209@gmail.com>
2022-08-28 19:44:49 +0530

    add

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {StateProvider} from './components/Provider/StateProvider'
 import reducer, { initialState } from './reducer';
-import {BrowserRouter} from "react-router-dom";
  
 
 // ReactDOM.render(
@@ -21,9 +20,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <StateProvider reducer={reducer} initialState={initialState} >
-        <BrowserRouter basename="/Anant-Motors">
         <App />
-        </BrowserRouter>
         </StateProvider>
     </React.StrictMode>
 
